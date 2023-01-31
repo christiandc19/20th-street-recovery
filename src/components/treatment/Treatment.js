32c71814fb4f3795af5ce24a9861e89340fdcdde
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-01-31 14:32:15 -0800

    styling

--- src/components/treatment/Treatment.js
+++ src/components/treatment/Treatment.js
@@ -12,18 +12,6 @@ import Wave from '../../assets/waves.svg'
 
 
 import { FaRegStar } from 'react-icons/fa';
-import { RiMovieFill } from 'react-icons/ri';
-import { GiBowlingStrike } from 'react-icons/gi';
-import { FaUmbrellaBeach } from 'react-icons/fa';
-import { FaGolfBall } from 'react-icons/fa';
-import { IoIosBonfire } from 'react-icons/io';
-import { GiSparkles } from 'react-icons/gi';
-import { MdSportsSoccer } from 'react-icons/md';
-import { FaHiking } from 'react-icons/fa';
-import { MdPool } from 'react-icons/md';
-import { MdVolunteerActivism } from 'react-icons/md';
-
-
 
 const Treatment = () => {
     return (
@@ -36,103 +24,136 @@ const Treatment = () => {
             <div className='hero-divider'>
                 <img src={Wave} loading="lazy" repeat-x/>
             </div>
-                    <div className="treatment-content">
-                        <Fade right>
-                            <h1>TREATMENT</h1>
-                        </Fade>
-                        {/* <Fade left>
-                            <p>All treatment clients will have a recovery/treatment plan developed within 30 days of admission. Measurement and re-assessment of treatment plan progress happen on a weekly basis through the weekly summary. In the weekly summary, the drug and alcohol counselor will ensure that action steps to be taken by client or counselor are on target and that the action step achievements will enable resolution of the objective. All treatment plans are reviewed by and signed off on by the clinical director. </p>
-                        </Fade> */}
-                    </div>
-                </div>
+            <div className="treatment-content">
+                <Fade right>
+                    <h1>TREATMENT</h1>
+                </Fade>
+            </div>
+        </div>
 
-            {/* -------------------------------- SECTION 1 -------------------------------------- */}
+{/* -------------------------------- SECTION 1 -------------------------------------- */}
 
-<div className='treatment-title'>
-<h1>KEY AREAS/FUNDAMENTALS</h1>
-</div>
+
 
     <div className='treatment-wrap'>
-
-    <div className="treatment-box treatment-left">
-        <br/>
-        <p>The treatment plan will be developed between client and counselor from key information gleaned from the assessment and intake process. This treatment plan will be goal and action oriented with objective and measurable criteria. Each treatment plan will have a statement of problem(s) with which the client identifies, statement of objectives to be reached that addressees each problem, action steps for client and counselor to ensure that objective will be completed.
-        <br/><br/>
-        Counselor and client will work together to compose a treatment plan with clear objectives and goals based on key areas intake assessment. It will include: challenges that the client may encounter during recovery process, clear methods of navigating obstacles in recovery, action steps to be taken by the program and/or client to overcome the challenges of recovery.
-        </p>
-        <br />
-    </div>
-
-    <div className="treatment-box box">
-        <img src={Service1} width="600px" height="350px" alt="Palm Springs Rehab Logo" loading="lazy" />
-    </div>
+        <div className="treatment-box treatment-left">
+            <div className='treatment-title'>
+                <h1>INPATIENT PROGRAMS</h1>
+            </div><br/>
+                <p>Our inpatient programs provide a higher level of care, accountability, and structure as you set off on your journey of recovery. 
+            <br/><br/>
+            <div className='treatment-title'>
+                <h4>DETOX</h4> <br/>
+            </div>
+                <ul>
+                    <li> <FaRegStar className='star'/> As the first step before the official treatment phase, our detox program offers immediate and intense support to get clean and stay clean</li> <br/>
+                    <li> <FaRegStar className='star'/> All services, monitoring, and amenities provided for the entirety of your stay: medication, followed by monitored detox care and group sessions. </li>
+                </ul>
+                </p>
+            </div>
+        <div className="treatment-box box">
+            <img src={Service1} width="600px" height="350px" alt="Palm Springs Rehab Logo" loading="lazy" />
+        </div>
 </div>
 
-<div className='treatment-title'>
-<h1>REVISIONS</h1>
-</div>
+
+
 <div className='treatment-wrap'>
     
     <div className="treatment-box box">
-        <img src={Service2} width="600px" height="350px" alt="Palm Springs Rehab Logo" loading="lazy" />
+        <img src={Service2} width="600px" height="400px" alt="Palm Springs Rehab Logo" loading="lazy" />
     </div>
 
 
     <div className="treatment-box treatment-left">
-        <br/>
-        <p>
-        Some areas to be examined may be personal relationships, work, anger management, lack of power, financial burdens, separation from children, past trauma as it relates to present, school, etc. 
-        </p>
-        <br />
-        <ul>
-            <li>
-            <FaRegStar className='star'/> Counselor and client will set clear and tangible goals regarding above mentioned obstacles.
-            </li><br/>
-            <li>
-            <FaRegStar className='star'/> Counselor and client will brainstorm potential hurdles as client attempts to accomplish goals.
-            </li><br/>
-            <li>
-            <FaRegStar className='star'/> Counselor and client discuss systems for working through hurdles. Counselor and client will — upon following weekly session — discuss successes and setbacks for set goals.
-            </li>
-            </ul>
+        
+            <div className='treatment-title'>
+                <h4>RESIDENTIAL</h4> <br/>
+            </div>
+                <ul>
+                    <li> <FaRegStar className='star'/> Looking for additional structure during detox? Step into a closely monitored healing environment and stay as long as you need.</li> <br/>
+                    <li> <FaRegStar className='star'/> 24 hour supervision as needed from our staff. </li><br/>
+                    <li> <FaRegStar className='star'/> Can be the best approach if you are prone to relapse, or your normal life involves the presence of substances that could hinder a successful ongoing recovery. </li>
+                </ul><br/>
+                <div className='treatment-title'>
+                <h4>PARTIAL HOSPITALIZATION PROGRAM</h4> <br/>
+            </div>
+                <ul>
+                    <li> <FaRegStar className='star'/> The step between full-time inpatient and outpatient, our Partial Hospitalization Program (or ‘day rehab’) helps you transition smoothly back into your normal life at your pace.</li> <br/>
+                    <li> <FaRegStar className='star'/> Take part in treatment and therapy during the day and spend your nights in the comfort of your own home.</li><br/>
+                </ul>
     </div>
 </div>
 
-<br />
-<br />
 
 
-<div className='activity-container'>
-
-                        <div className='activity-section'>
-                            <div className='recreation container'>
-                                <p>
-                                    As mentioned above, the weekly summary reflective of the session between client and counselor/therapist will measure each treatment plan goal on a weekly basis. The client will also initial the weekly summary as this is a review of the treatment plan goals and objectives. Furthermore, clients may have new issues come up during treatment that were not identified at the time of assessment. Drug and alcohol counselors/therapists will develop new treatment plan goals as needed using this protocol whenever necessary.
-                                </p>
-                            </div>
-                        </div>
+<br/>
+<br/>
 
 
-            {/* ------------------------------ RECREATION --------------------------- */}
-            <h1 className='activity-header'> RECREATIONS </h1>                                
 
-                <div className='activities'>
-                    
-                        <p><RiMovieFill /><span> MOVIES</span></p><br />
-                        <p><GiBowlingStrike /> <span> BOWLING</span></p><br />
-                        <p><FaUmbrellaBeach /> <span> BEACH/LAKE ACTIVITIES</span></p><br />
-                        <p><FaGolfBall /> <span> MINIATURE GOLF</span></p><br />
-                        <p><IoIosBonfire /> <span> BONFIRE</span></p><br />
-                        <p><GiSparkles /> <span> AMUSEMENT PARKS</span></p><br />
-                        <p><MdSportsSoccer /> <span> SPORTING EVENTS</span></p><br />
-                        <p><FaHiking /> <span> HIKING</span></p><br />
-                        <p><MdPool /> <span> POOL</span></p><br />
-                        <p><MdVolunteerActivism /> <span> FUNDRAISERS</span> </p><br />
-                        <br />
-                </div>
+<div className='treatment-wrap'>
+        <div className="treatment-box treatment-left">
+            <div className='treatment-title'>
+                <h1>OUTPATIENT PROGRAMS</h1>
+            </div><br/>
+                <p>
+            <div className='treatment-title'>
+                <h4>PHP (Mental Health and Addiction)</h4> <br/>
+            </div>
+                <ul>
+                    <li> <FaRegStar className='star'/> A more intense, comprehensive treatment approach than IOP, available every day of the week. </li> <br/>
+                    <li> <FaRegStar className='star'/> For those with families and responsibilities at home who seek ongoing treatment. </li><br/>
+                    <li> <FaRegStar className='star'/> Spend your days in recovery with our team your nights at home. </li><br/>
+                    <li> <FaRegStar className='star'/> One-on-one support available as needed. </li>
+                </ul>
+                </p>
+            </div>
+        <div className="treatment-box box">
+            <img src={Service1} width="600px" height="350px" alt="Palm Springs Rehab Logo" loading="lazy" />
+        </div>
+</div>
 
+
+
+
+
+
+<div className='treatment-wrap'>
+    
+    <div className="treatment-box box">
+        <img src={Service2} width="600px" height="400px" alt="Palm Springs Rehab Logo" loading="lazy" />
     </div>
-          
+
+
+    <div className="treatment-box treatment-left">
+        
+            <div className='treatment-title'>
+                <h4>IOP (Mental Health and Addiction)</h4> <br/>
+            </div>
+                <ul>
+                    <li> <FaRegStar className='star'/> Several hours of group therapy each day along with continued, personalized support and careful monitoring by our in-house physicians.</li> <br/>
+                    <li> <FaRegStar className='star'/> Take part in our unique jobs program. We equip you with all the tools you need to succeed in finding the right job. </li><br/>
+                </ul><br/>
+                <div className='treatment-title'>
+                <h4>OP (Mental Health and Addiction)
+</h4> <br/>
+            </div>
+                <ul>
+                    <li> <FaRegStar className='star'/> Get continued support as needed with our outpatient programs.</li> <br/>
+                    <li> <FaRegStar className='star'/> Attend weekly group and counseling as you move back to your normal life.</li><br/>
+                </ul>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
 
 
         <ContactForm />
