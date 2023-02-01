1c543949b28d720449ed34c27dbca985fbbad0a5
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-02-01 09:12:44 -0800

    styling

---
+++ src/components/gallery/Gallery.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Gallery.css'
+
+const Gallery = () => {
+
+    
+
+  return (
+
+<div>
+    <h1>In Progress</h1>
+</div>
+
+
+  )
+}
+
+export default Gallery
